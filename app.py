31831a4c0265a11a307f5bae318f7b20282f91e0
MateBar <barbirm@mail.com>
2021-07-31 04:03:40 +0100

    Added functionality for log out, and organised navbar

--- app.py
+++ app.py
@@ -86,10 +86,22 @@ def login():
 
 @app.route("/profile/<username>", methods=["GET", "POST"])
 def profile(username):
-    # grab the session user's username from db
+    # Takes the session user's username from database
     username = mongo.db.users.find_one(
         {"username": session["user"]})["username"]
-    return render_template("profile.html", username=username)
+
+    if session["user"]:
+        return render_template("profile.html", username=username)
+
+    return redirect(url_for("login"))
+
+
+@app.route("/logout")
+def logout():
+    # Removes the user from session cookie
+    flash("You have logged out successfully")
+    session.pop("user")
+    return redirect(url_for("login"))
 
 
 if __name__ == "__main__":
